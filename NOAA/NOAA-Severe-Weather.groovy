2a204b1c0108f12f05e035d6abafab8b0077e491
Imnotbob <Imnot_bob@hotmail.com>
2022-11-18 13:13:53 -0500

    more clean

--- NOAA/NOAA-Severe-Weather.groovy
+++ NOAA/NOAA-Severe-Weather.groovy
@@ -32,7 +32,6 @@ import java.text.SimpleDateFormat
 //import java.util.Date
 //import groovy.time.*
 
-
 @Field static final String appVersionFLD ='4.0.020'
 @Field static final String sNULL         =(String)null
 @Field static final String sBLANK        =''
@@ -76,7 +75,6 @@ preferences{
 	page ((sNM): "AdvConfigPage", title: sBLANK, install: false, uninstall: false, nextPage: "mainPage")
 	page ((sNM): "RestrictionsPage", title: sBLANK, install: false, uninstall: false, nextPage: "mainPage")
 	page ((sNM): "SettingsPage", title: sBLANK, install: false, uninstall: true, nextPage: "mainPage")
-
 }
 
 def mainPage(){
@@ -376,7 +374,6 @@ def SettingsPage(){
 			paragraph "Change applicaiton label?"
 			label ((sNM): sNM, title: "Name", state: "complete", defaultValue: app.name, required: false)
 		}
-
 	}
 }
 
@@ -598,7 +595,8 @@ void finishAlertMsg(Map result){
 			logTrace "filtered: ${msgMap==null ? sTRUE : sFALSE} expired: ${expired}  alertexpires ${alertexpires}  replacedAt: $replacedAt useEnds: $useEnds    now: ${timestamp}  ${feat.properties.severity}  ${feat.properties.event}"
 
 			if(msgMap!=null){
-				if(!expired){
+				Boolean useexpired= (Boolean)settings.debug
+				if(!expired || useexpired){
 					Boolean isNewNotice; isNewNotice=false
 					if(mListofAlertsFLD.size() > 0){
 						Map fndMsg= mListofAlertsFLD.find{ Map it -> ((String)it.alertid).contains((String)msgMap.alertid)}
@@ -608,9 +606,10 @@ void finishAlertMsg(Map result){
 						isNewNotice=true
 						IsnewList=true
 					}
-					if((Boolean)settings.debug)
-						if(isNewNotice) logDebug "Valid ${msgMap.alertid} is new in ListofAlerts: ${IsnewList}"
-						else logDebug "Valid ${msgMap.alertid} exists in ListofAlerts"
+
+					if(expired && useexpired) logDebug "Using expired alerts"
+					if(isNewNotice) logDebug "Valid ${msgMap.alertid} is new in ListofAlerts: ${IsnewList}"
+					else logDebug "Valid ${msgMap.alertid} exists in ListofAlerts"
 
 					if(isNewNotice){ msgMap.alertPushed=false; msgMap.alertAnnounced=false}
 					msgMap.expired=expired
@@ -1111,7 +1110,6 @@ void pushNow(String ialertmsg, Boolean repeatCheck){
 	}
 }
 
-
 List<Map> getTile(){
 	String myId=app.getId()
 	if(!ListofAlertsFLD[myId] && (List)state.ListofAlerts) ListofAlertsFLD[myId]=(List)state.ListofAlerts // on hub restart or code reload
@@ -1144,9 +1142,9 @@ void buildEventsListFrc(){
 }
 
 @Field static final List<String> eventListFLD=[
-"911 Telephone Outage Emergency", "Administrative Message", "Air Quality Alert", "Air Stagnation Advisory", "Arroyo And Small Stream Flood Advisory", "Ashfall Advisory", "Ashfall Warning", "Avalanche Advisory", "Avalanche Warning", "Avalanche Watch", "Beach Hazards Statement", "Blizzard Warning", "Blizzard Watch", "Blowing Dust Advisory", "Blowing Dust Warning", "Brisk Wind Advisory", "Child Abduction Emergency", "Civil Danger Warning", "Civil Emergency Message", "Coastal Flood Advisory", "Coastal Flood Statement", "Coastal Flood Warning", "Coastal Flood Watch", "Dense Fog Advisory", "Dense Smoke Advisory", "Dust Advisory", "Dust Storm Warning", "Earthquake Warning", "Evacuation - Immediate", "Excessive Heat Warning", "Excessive Heat Watch", "Extreme Cold Warning", "Extreme Cold Watch", "Extreme Fire Danger", "Extreme Wind Warning", "Fire Warning", 
-"Fire Weather Watch", "Flash Flood Statement", "Flash Flood Warning", "Flash Flood Watch", "Flood Advisory", "Flood Statement", "Flood Warning", "Flood Watch", "Freeze Warning", "Freeze Watch", "Freezing Fog Advisory", "Freezing Rain Advisory", "Freezing Spray Advisory", "Frost Advisory", "Gale Warning", "Gale Watch", "Hard Freeze Warning", "Hard Freeze Watch", "Hazardous Materials Warning", "Hazardous Seas Warning", "Hazardous Seas Watch", "Hazardous Weather Outlook", "Heat Advisory", "Heavy Freezing Spray Warning", "Heavy Freezing Spray Watch", "High Surf Advisory", "High Surf Warning", "High Wind Warning", "High Wind Watch", "Hurricane Force Wind Warning", "Hurricane Force Wind Watch", "Hurricane Local Statement", "Hurricane Warning", "Hurricane Watch", "Hydrologic Advisory", "Hydrologic Outlook", "Ice Storm Warning", "Lake Effect Snow Advisory", "Lake Effect Snow Warning", "Lake Effect Snow Watch", "Lake Wind Advisory", "Lakeshore Flood Advisory", "Lakeshore Flood Statement", "Lakeshore Flood Warning", 
-"Lakeshore Flood Watch", "Law Enforcement Warning", "Local Area Emergency", "Low Water Advisory", "Marine Weather Statement", "Nuclear Power Plant Warning", "Radiological Hazard Warning", "Red Flag Warning", "Rip Current Statement", "Severe Thunderstorm Warning", "Severe Thunderstorm Watch", "Severe Weather Statement", "Shelter In Place Warning", "Short Term Forecast", "Small Craft Advisory", "Small Craft Advisory For Hazardous Seas", "Small Craft Advisory For Rough Bar", "Small Craft Advisory For Winds", "Small Stream Flood Advisory", "Snow Squall Warning", "Special Marine Warning", "Special Weather Statement", "Storm Surge Warning", "Storm Surge Watch", "Storm Warning", "Storm Watch", "Test", "Tornado Warning", "Tornado Watch", "Tropical Depression Local Statement", "Tropical Storm Local Statement", "Tropical Storm Warning", 
+"911 Telephone Outage Emergency", "Administrative Message", "Air Quality Alert", "Air Stagnation Advisory", "Arroyo And Small Stream Flood Advisory", "Ashfall Advisory", "Ashfall Warning", "Avalanche Advisory", "Avalanche Warning", "Avalanche Watch", "Beach Hazards Statement", "Blizzard Warning", "Blizzard Watch", "Blowing Dust Advisory", "Blowing Dust Warning", "Brisk Wind Advisory", "Child Abduction Emergency", "Civil Danger Warning", "Civil Emergency Message", "Coastal Flood Advisory", "Coastal Flood Statement", "Coastal Flood Warning", "Coastal Flood Watch", "Dense Fog Advisory", "Dense Smoke Advisory", "Dust Advisory", "Dust Storm Warning", "Earthquake Warning", "Evacuation - Immediate", "Excessive Heat Warning", "Excessive Heat Watch", "Extreme Cold Warning", "Extreme Cold Watch", "Extreme Fire Danger", "Extreme Wind Warning", "Fire Warning",
+"Fire Weather Watch", "Flash Flood Statement", "Flash Flood Warning", "Flash Flood Watch", "Flood Advisory", "Flood Statement", "Flood Warning", "Flood Watch", "Freeze Warning", "Freeze Watch", "Freezing Fog Advisory", "Freezing Rain Advisory", "Freezing Spray Advisory", "Frost Advisory", "Gale Warning", "Gale Watch", "Hard Freeze Warning", "Hard Freeze Watch", "Hazardous Materials Warning", "Hazardous Seas Warning", "Hazardous Seas Watch", "Hazardous Weather Outlook", "Heat Advisory", "Heavy Freezing Spray Warning", "Heavy Freezing Spray Watch", "High Surf Advisory", "High Surf Warning", "High Wind Warning", "High Wind Watch", "Hurricane Force Wind Warning", "Hurricane Force Wind Watch", "Hurricane Local Statement", "Hurricane Warning", "Hurricane Watch", "Hydrologic Advisory", "Hydrologic Outlook", "Ice Storm Warning", "Lake Effect Snow Advisory", "Lake Effect Snow Warning", "Lake Effect Snow Watch", "Lake Wind Advisory", "Lakeshore Flood Advisory", "Lakeshore Flood Statement", "Lakeshore Flood Warning",
+"Lakeshore Flood Watch", "Law Enforcement Warning", "Local Area Emergency", "Low Water Advisory", "Marine Weather Statement", "Nuclear Power Plant Warning", "Radiological Hazard Warning", "Red Flag Warning", "Rip Current Statement", "Severe Thunderstorm Warning", "Severe Thunderstorm Watch", "Severe Weather Statement", "Shelter In Place Warning", "Short Term Forecast", "Small Craft Advisory", "Small Craft Advisory For Hazardous Seas", "Small Craft Advisory For Rough Bar", "Small Craft Advisory For Winds", "Small Stream Flood Advisory", "Snow Squall Warning", "Special Marine Warning", "Special Weather Statement", "Storm Surge Warning", "Storm Surge Watch", "Storm Warning", "Storm Watch", "Test", "Tornado Warning", "Tornado Watch", "Tropical Depression Local Statement", "Tropical Storm Local Statement", "Tropical Storm Warning",
 "Tropical Storm Watch", "Tsunami Advisory", "Tsunami Warning", "Tsunami Watch", "Typhoon Local Statement", "Typhoon Warning", "Typhoon Watch", "Urban And Small Stream Flood Advisory", "Volcano Warning", "Wind Advisory", "Wind Chill Advisory", "Wind Chill Warning", "Wind Chill Watch", "Winter Storm Warning", "Winter Storm Watch", "Winter Weather Advisory"
 ]
 
@@ -1214,7 +1212,6 @@ Map getResponseURL(Boolean async=false){
 
 	state.wxURI=wxURI
 	logDebug "URI: <a href='${wxURI}' target=_blank>${wxURI}</a>"
-
 
 	logInfo "Connecting to weather.gov service."
 	Map requestParams =	[
